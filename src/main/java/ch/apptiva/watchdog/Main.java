23b4731d35e97abf07aec5998174cb454fa32d26
Roman Schaller <roman.schaller@gmail.com>
2016-12-16 15:17:52 +0100

    get token from application argument or from environment variable

--- src/main/java/ch/apptiva/watchdog/Main.java
+++ src/main/java/ch/apptiva/watchdog/Main.java
@@ -15,54 +15,64 @@ import com.ullink.slack.simpleslackapi.listeners.SlackMessagePostedListener;
 
 public class Main {
 
-  public static void main(String[] args)
-      throws InterruptedException, IOException, URISyntaxException {
-    String token = args[0];
-    SlackSession slackSession = SlackSessionFactory.createWebSocketSlackSession(token);
-    slackSession.connect();
-    Watcher watcher = new Watcher();
-    watcher.load();
-    MessageDispatcher dispatcher = new MessageDispatcher(watcher);
+    public static void main(String[] args)
+            throws InterruptedException, IOException, URISyntaxException {
+        String token = getToken(args);
+        SlackSession slackSession = SlackSessionFactory.createWebSocketSlackSession(token);
+        slackSession.connect();
+        Watcher watcher = new Watcher();
+        watcher.load();
+        MessageDispatcher dispatcher = new MessageDispatcher(watcher);
 
-    slackSession.addMessagePostedListener(new SlackMessagePostedListener() {
-      @Override
-      public void onEvent(SlackMessagePosted event, SlackSession session) {
-        if (!event.getSender().isBot()) {
-          dispatcher.dispatch(event, session);
-        }
-      }
-    });
+        slackSession.addMessagePostedListener(new SlackMessagePostedListener() {
+            @Override
+            public void onEvent(SlackMessagePosted event, SlackSession session) {
+                if (!event.getSender().isBot()) {
+                    dispatcher.dispatch(event, session);
+                }
+            }
+        });
 
-    while (true) {
-      Thread.sleep(60 * 1000);
-      watcher.watch(new WatchEventListener() {
-        @Override
-        public void stateChanged(WatchStateEnum from, WatchStateEnum to, WatchedURI watchedURI) {
-          String message;
-          if (OK.equals(to) && UNKNOWN.equals(from)) {
-            message = watchedURI.getUri().toString() + " ist erreichbar und antwortet.";
-          } else if (OK.equals(to) && NOK.equals(from)) {
-            message = "Hurra! " + watchedURI.getUri().toString() + " ist wieder erreichbar.";
-          } else if (NOK.equals(to)) {
-            message = "Uiiii! " + watchedURI.getUri().toString()
-                + " ist nicht erreichbar. Du schaust besser mal vorbei, bevor es Probleme gibt!\n"
-                + "Details: " + watchedURI.getErrorCause();
-          } else {
-            message = "Der Status von " + watchedURI.getUri().toString() + " änderte von " + from
-                + " nach " + to + ".\n" + "Details: " + watchedURI.getErrorCause();
-          }
-          SlackChannel channel = slackSession.findChannelByName(watchedURI.getChannelRespond());
-          if (channel == null) {
-            channel = slackSession.findChannelById(watchedURI.getChannelRespond());
-          }
-          slackSession.sendMessage(channel, message);
+        while (true) {
+            Thread.sleep(60 * 1000);
+            watcher.watch(new WatchEventListener() {
+                @Override
+                public void stateChanged(WatchStateEnum from, WatchStateEnum to, WatchedURI watchedURI) {
+                    String message;
+                    if (OK.equals(to) && UNKNOWN.equals(from)) {
+                        message = watchedURI.getUri().toString() + " ist erreichbar und antwortet.";
+                    } else if (OK.equals(to) && NOK.equals(from)) {
+                        message = "Hurra! " + watchedURI.getUri().toString() + " ist wieder erreichbar.";
+                    } else if (NOK.equals(to)) {
+                        message = "Uiiii! " + watchedURI.getUri().toString()
+                                + " ist nicht erreichbar. Du schaust besser mal vorbei, bevor es Probleme gibt!\n"
+                                + "Details: " + watchedURI.getErrorCause();
+                    } else {
+                        message = "Der Status von " + watchedURI.getUri().toString() + " änderte von " + from
+                                + " nach " + to + ".\n" + "Details: " + watchedURI.getErrorCause();
+                    }
+                    SlackChannel channel = slackSession.findChannelByName(watchedURI.getChannelRespond());
+                    if (channel == null) {
+                        channel = slackSession.findChannelById(watchedURI.getChannelRespond());
+                    }
+                    slackSession.sendMessage(channel, message);
+                }
+            });
+            if (dispatcher.shutdownRequested()) {
+                slackSession.disconnect();
+                break;
+            }
         }
-      });
-      if (dispatcher.shutdownRequested()) {
-        slackSession.disconnect();
-        break;
-      }
     }
-  }
+
+    private static String getToken(String[] args) {
+        if (args.length > 0 && args[0] != null) {
+            return args[0];
+        } else if (System.getenv("slacktoken") !=null) {
+            return System.getenv("slacktoken");
+        } else {
+            throw new IllegalStateException("I need a slack token. Either by program argument or by environment variable 'slacktoken'.");
+        }
+    }
 
 }
